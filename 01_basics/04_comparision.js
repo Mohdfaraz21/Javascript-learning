a0a455eb97b9f8e780bccae9c9af4571d38a1910
Mohdfaraz21 <96872512+Mohdfaraz21@users.noreply.github.com>
2023-11-07 10:55:14 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+// in comparision js convert null to number, treating it as 0 thats why (line 13 is true) and (line 10 is false)
+console.log(null >= 0);  
+
+// AVIOD THESE TYPE OF COMPARISION
